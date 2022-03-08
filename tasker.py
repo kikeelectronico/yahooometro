226e2a88265659d855328de3b875ddd25d1a98cb
enrique.gomez <enrique.gomez@bq.com>
2022-03-08 21:31:30 +0100

    tasker

---
+++ tasker.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import time
+import json
+
+if __name__ == "__main__":
+  while True:
+    now = datetime.now()
+    hour = now.hour
+    minute = now.minute
+    if hour == 21 and minute == 25:
+      f = open('./data/users.json', 'r')
+      users = json.loads(f.read())['users']
+      f.close()
+      for i, user in enumerate(users):
+        users[i]['count'] = 0
+      f = open('./data/users.json', 'w')
+      f.write(json.dumps({"users": users}))
+      f.close()
+    time.sleep(3600)
